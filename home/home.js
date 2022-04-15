665224f0bed0fea587e504f8e38efc0d7ae9b05a
Theodhor Pandeli <theodhorpandeli@gmail.com>
2022-04-15 18:44:09 +0200

    Fixes

--- home/home.js
+++ home/home.js
@@ -1,7 +1,7 @@
-          //lexo vleren qe ka key "name" dhe ruaje tek variabli savedValue
-        	var savedValue = localStorage.getItem("emaili");
-          
-          //printo vleren qe ka savedValue
-        	console.log(savedValue);
+//lexo vleren qe ka key "name" dhe ruaje tek variabli savedValue
+var savedValue = localStorage.getItem("emaili");
 
-          document.getElementById("Titulli").innerHTML = savedValue;
+//printo vleren qe ka savedValue
+console.log(savedValue);
+
+document.getElementById("Titulli").innerHTML = savedValue;
